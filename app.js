46b1162c148212b6ce720fa9a89f02332816425e
Mars <martinianol@outlook.com>
2021-04-28 18:16:15 -0300

    styles added

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require ('express');
 const app = express ();
 const path = require ('path');
 
-const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
+const publicFolderPath = path.resolve(__dirname, './public');
+app.use(express.static(publicFolderPath));
 
 
 app.get ('/', (req, res) => {
