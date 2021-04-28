fe362f67f851b2d01a8c2dbd4ee0326333ebb2c2
Mars <martinianol@outlook.com>
2021-04-28 16:53:42 -0300

    Todo listo para empezar a llenar el contenido

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require ('express');
 const app = express ();
 const path = require ('path');
 
+const publicPath = path.resolve(__dirname + '/public');
+app.use(express.static(publicPath));
+
 app.get ('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/views/home.html'))
-})
+res.sendFile(path.resolve(__dirname + '/views/home.html'))
+});
 
 app.listen(3030, () => console.log ('Server up and running on port 3030'));
 
